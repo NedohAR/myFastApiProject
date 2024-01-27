8bfebf8192dce13b105cf13c858e8b31820b4150
ArsenDev <arsen.nedogibchencko@gmail.com>
2024-01-27 20:18:14 +0200

    add front and back

---
+++ api/app/main.py
@@ -0,0 +1,104 @@
+from typing import Union
+
+import json
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+import pymysql
+from dotenv import load_dotenv
+import os
+
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.environ.get("MYSQL_PORT", 3306)
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.environ.get("MYSQL_DB", "users")
+
+
+def connect():
+    return pymysql.connect(
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DB
+    )
+
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    name: str
+    email: str
+    password: str
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello"}
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exc: Exception):
+    print(f"Exception: {exc}")
+    return JSONResponse(
+        status_code=500,
+        content={"message": "Internal Server Error", "error": str(exc)}
+    )
+
+
+@app.post("/create_user")
+async def create_user(user: User):
+    conn = connect()
+    cursor = conn.cursor()
+    query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+    cursor.execute(query, (user.name, user.email, user.password))
+    conn.commit()
+    conn.close()
+    return {"message": "User created successfully"}
+
+
+@app.put("/update_user/{user_id}")
+async def update_user(user_id: int, user: User):
+    conn = connect()
+    cursor = conn.cursor()
+    query = "UPDATE users SET name=%s, email=%s, password=%s WHERE id=%s"
+    cursor.execute(query, (user.name, user.email, user.password, user_id))
+    conn.commit()
+    conn.close()
+    return {"message": "User updated successfully"}
+
+
+@app.delete("/delete_user/{user_id}")
+async def delete_user(user_id: int):
+    conn = connect()
+    cursor = conn.cursor()
+    query = "DELETE FROM users WHERE id=%s"
+    cursor.execute(query, (user_id,))
+    conn.commit()
+    conn.close()
+    return {"message": "User deleted successfully"}
+
+
+@app.get("/get_users")
+async def get_users():
+    conn = connect()
+    cursor = conn.cursor()
+    query = "SELECT name, email FROM users"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    conn.close()
+    return {"users": [{"name": row[0], "email": row[1]} for row in rows]}
---
+++ frontend/src/App.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import api from './api';
+import './App.css';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password } = formData;
+
+  const fetchUsers = async () => {
+    try {
+      const response = await api.get('/get_users');
+      setUsers(response.data.user);
+    } catch (error) {
+      console.error('Error fetching users:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const createUser = async () => {
+    try {
+      await api.post('/create_user', formData);
+      setFormData({
+        name: '',
+        email: '',
+        password: '',
+      });
+      fetchUsers();
+    } catch (error) {
+      console.error('Error creating user:', error.message);
+    }
+  };
+
+  const updateUser = async (userId) => {
+    try {
+      await api.put(`/update_user/${userId}`, formData);
+      fetchUsers();
+    } catch (error) {
+      console.error('Error updating user:', error.message);
+    }
+  };
+
+  const deleteUser = async (userId) => {
+    try {
+      await api.delete(`/delete_user/${userId}`);
+      fetchUsers();
+    } catch (error) {
+      console.error('Error deleting user:', error.message);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>User Management App</h1>
+
+      <form>
+        <label>Name:</label>
+        <input type="text" value={name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} required />
+
+        <label>Email:</label>
+        <input type="email" value={email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} required />
+
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setFormData({ ...formData, password: e.target.value })} required />
+
+        <button type="button" onClick={createUser}>
+          Create User
+        </button>
+      </form>
+
+      <div>
+        <h2>Users List</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {`${user.name} - ${user.email}`}
+              <button type="button" onClick={() => updateUser(user.id)}>
+                Update
+              </button>
+              <button type="button" onClick={() => deleteUser(user.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
